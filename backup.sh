acda93f313f9b0133864792b042b33c0e356574b
AlecDakin <AlecDakin@users.noreply.github.com>
2019-06-19 14:37:22 +0100

    Delete files older than 90 days

--- backup.sh
+++ backup.sh
@@ -7,14 +7,20 @@ LOGLOC=$DIRLOC/logs/backup_$(date +%Y-%m-%d).log
 USBDRIVE="$( cat /etc/mtab | grep $DIRLOC | wc -l )"
 
 if [ "$USBDRIVE" -eq 1 ]; then
+	# delete old logs and backups - more than 90 days
+	find $DIRLOC/logs/*.log -mtime +90 -type f -delete
+	find $DIRLOC/backup/backup* -mtime +90 -type d -ls -exec rm -rv {} +
         # Run the rsync
-        rsync -brtvhP --modify-window=2 --stats --delete-delay --force --log-file=$LOGLOC --backup-dir=$BACKUPLOC /storage/ $DIRLOC/live/
+        rsync -brtvhP --modify-window=2 --stats --delete-delay --delete-excluded --force --exclude=spool --exclude="*.tmp" --exclude="*~$*" --log-file=$LOGLOC --backup-dir=$BACKUPLOC /storage/ $DIRLOC/live/
 	#sync to flush writes to USB device
 	sync
-	#echo $DIRLOC $USBDRIVE
-	telegram-send -g "Backup complete, see attached log"
-	telegram-send -g -f $LOGLOC
-else 
-	telegram-send -g "*** No USB drive attached for Backup ***"
+	echo $DIRLOC $USBDRIVE
+	telegram-send -g "$(hostname) Backup complete, see attached log"
+	if [ ! -f "$LOGLOC" ]; then
+		telegram-send -g "$(hostname)  *** ERROR NO BACKUP LOG ***"
+	else
+		telegram-send -g "$(hostname)" -f "$LOGLOC"
+	fi
+else
+	telegram-send -g "$(hostname) *** No USB drive attached for Backup ***"
 fi
-
